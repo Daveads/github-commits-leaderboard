1e909bc58eb27ee940f0fe610b2e05f877ee42e5
daveads <the.daveads@gmail.com>
2023-01-19 08:38:20 +0100

    let's get started

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+from datetime import date, datetime
 
 with open("challenger.txt", 'r') as f:
     text = f.read()
 
 githubUsername = text.split()
-
+start_date = date(datetime.now().year, 1,1)
 data = []
 
-
 for i in githubUsername:
-    URL = ("https://github.com/%s?tab=overview&from=2023-01-01&to=2023-01-16" % i)
+    URL = ("https://github.com/%s?tab=overview&from=%s&to=%s" % (i, start_date, date.today()))
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
 
@@ -58,6 +58,6 @@ commits = get_commit_per_user(data)
 Lead_data = leaderboard_sort(commits, data)
 
 
-print(" Username  >>  Name  >>  commits ")
+print(f"  Username  >>>  Name  >>>  contributions in {datetime.now().year}")
 for count, i in enumerate(Lead_data):
     print(count, i)
