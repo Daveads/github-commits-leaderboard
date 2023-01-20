58c9d80a5212fdcab89e2762b6cbc6c88d1065f1
daveads <the.daveads@gmail.com>
2023-01-20 21:21:07 +0100

    not fast

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
-import requests
+import asyncio
 from bs4 import BeautifulSoup
 import re
+import aiofiles
+import aiohttp
+
+import time
 
 from datetime import date, datetime
 
-with open("challenger.txt", 'r') as f:
-    text = f.read()
 
-githubUsername = text.split()
 start_date = date(datetime.now().year, 1,1)
-data = []
 
-for i in githubUsername:
-    URL = ("https://github.com/%s?tab=overview&from=%s&to=%s" % (i, start_date, date.today()))
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
 
-    results = soup.find("h2", {"class": "f4 text-normal mb-2"}).text.strip().replace("\n","")
+async def userlist():
+    async with aiofiles.open("challenger.txt", 'r') as f:
+        text = await f.read()
+    return text
+
+
+
+async def get_user_data(user):
+    async with aiohttp.ClientSession() as session:
+            async with session.get("https://github.com/%s?tab=overview&from=%s&to=%s" % (user, start_date, date.today())) as response:
+                page = await response.text()
+
+    return page
+
+async def user_data(users):
+    data = []
+    for user in users:
+
+        page =  await  get_user_data(user)                
+        soup = BeautifulSoup(page, "html.parser")
+
+        results = soup.find("h2", {"class": "f4 text-normal mb-2"}).text.strip().replace("\n","")
     
-    fullName = soup.find("title").text
-    namelgth = len(fullName) - 8
+        fullName = soup.find("title").text
+        namelgth = len(fullName) - 8
 
-    strfmt = re.sub("\s+", " ", results)
+        strfmt = re.sub("\s+", " ", results)
 
-    c = strfmt.split()
+        c = strfmt.split()
 
-    userBoard  = f"{fullName[:namelgth]} {c[0]}"
+        userBoard  = f"{fullName[:namelgth]} {c[0]}"
     
-    data.append(userBoard)
+        data.append(userBoard)
+
+    return data
 
 
-#print(data)
 
 def get_commit_per_user(data):
     commit_list = []
@@ -53,11 +71,31 @@ def leaderboard_sort(commits, data):
     return data
 
 
-commits = get_commit_per_user(data)
-
-Lead_data = leaderboard_sort(commits, data)
 
 
-print(f"  Username  >>>  Name  >>>  contributions in {datetime.now().year}")
-for count, i in enumerate(Lead_data):
-    print(count, i)
+
+async def main():
+
+
+    start_time = time.time()
+
+    githubusername = await userlist()
+
+    data_list = await user_data(githubusername.split())
+
+
+    commits = get_commit_per_user(data_list)
+
+    Lead_data = leaderboard_sort(commits, data_list)
+
+    end_time = time.time()
+
+    print(f"  Username  >>>  Name  >>>  contributions in {datetime.now().year}")
+    for count, i in enumerate(Lead_data):
+        print(count, i)
+
+    # Execution time
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time} seconds")
+
+asyncio.run(main())
