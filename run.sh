babcaafdc8d5cab77722736ef469b706ce23f1e0
daveads <the.daveads@gmail.com>
2023-07-22 10:25:46 +0100

    script

--- run.sh
+++ run.sh
@@ -4,22 +4,29 @@ RUN="To run this code  ./main.py "
 
 if command -v python > /dev/null; then
 
-	if [ -d "env" ]; then
-			source env/bin/activate
-			pip install -r requirement.txt > /dev/null
-			echo ">>> Packages Successfully Installed "
-			chmod +x main.py
-		    echo ">>> $RUN"
+    if [ -d "env" ]; then
+        source env/bin/activate
+        pip install -r requirement.txt > /dev/null
+        echo ">>> Packages Successfully Installed "
+        chmod +x main.py
+        echo ">>> $RUN"
 
-   else
-		   python -m venv env
-		   pip install --upgrade pip
-		   source env/bin/activate
-		   pip install -r requirement.txt
-		   echo ">>> Packages Successfully Installed "
-		   chmod +x main.py
-		   echo ">>> $RUN"
-	fi
+        # Run the main.py script and print its output
+        echo ">>> Output of ./main.py:"
+        ./main.py
+    else
+        python -m venv env
+        pip install --upgrade pip
+        source env/bin/activate
+        pip install -r requirement.txt
+        echo ">>> Packages Successfully Installed "
+        chmod +x main.py
+        echo ">>> $RUN"
+
+        # Run the main.py script and print its output
+        echo ">>> Output of ./main.py:"
+        ./main.py
+    fi
 
 else
     echo ">>> Error: Python is not installed"
