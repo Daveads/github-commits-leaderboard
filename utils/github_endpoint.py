7d4347bf041eb47c441e43ac14eec27852d23edb
daveads <the.daveads@gmail.com>
2023-07-22 10:34:29 +0100

    error handler

--- utils/github_endpoint.py
+++ utils/github_endpoint.py
@@ -3,9 +3,19 @@ from datetime import date, datetime
 
 start_date = date(datetime.now().year, 1, 1)
 
-async def get_user_data(user):
-    async with aiohttp.ClientSession() as session:
-        async with session.get("https://github.com/%s?tab=overview&from=%s&to=%s" % (user, start_date, date.today())) as response:
-            page = await response.text()
+class UserDataError(Exception):
+    pass
 
-    return page
+async def get_user_data(user):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://github.com/%s?tab=overview&from=%s&to=%s" % (user, start_date, date.today())) as response:
+                page = await response.text()
+
+        # Check if the response status code indicates success (status code 200)
+        if response.status != 200:
+            raise UserDataError(f"Failed to fetch data for user '{user}'. Status Code: {response.status}")
+
+        return page
+    except aiohttp.ClientError as e:
+        raise UserDataError(f"Error fetching data for user '{user}': {e}")
