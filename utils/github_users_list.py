4ea04109076e9a6cfe281739906a6a978b761162
daveads <the.daveads@gmail.com>
2023-07-22 11:04:09 +0100

    error handler

--- utils/github_users_list.py
+++ utils/github_users_list.py
@@ -1,9 +1,19 @@
 import aiofiles
 import os
 
+class UserListError(Exception):
+    pass
+
 async def userlist():
     root_path = os.path.dirname(os.path.abspath(__file__))  # Get the absolute path of the current file's directory
     file_path = os.path.join(root_path, f"../challenger.txt")  # Construct the correct relative path to challenger.txt
-    async with aiofiles.open(file_path, 'r') as f:
-        text = await f.read()
-    return text
+
+    try:
+        async with aiofiles.open(file_path, 'r') as f:
+            text = await f.read()
+        return text
+    except FileNotFoundError as e:
+        raise UserListError(f"File 'challenger.txt' not found: {e}")
+    
+    except Exception as e:
+        raise UserListError(f"Error reading 'challenger.txt': {e}")
