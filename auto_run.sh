5e5b436fcd77e933b47c583d2489d3e371e3303c
Ubuntu <ubuntu@ip-172-31-13-139.eu-north-1.compute.internal>
2023-08-01 07:54:02 +0000

    board update test**  2023-08-01

--- auto_run.sh
+++ auto_run.sh
@@ -3,10 +3,10 @@
 # crontab command:
 # 59 22 * * 0  /path/to/your_script.sh
 
-cd /path/repo_path/
+cd /home/ubuntu/git-commit-board
 git pull
 
 git add .
 today=`date +"%Y-%m-%d"`
-git commit -m "board update $today"
+git commit -m "board update test**  $today"
 git push origin main
