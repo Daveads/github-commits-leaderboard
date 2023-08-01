a90507f21c35608563294cffd111926fa4776067
daveads <the.daveads@gmail.com>
2023-08-01 08:00:02 +0000

    board update test** 2023-08-01

--- auto_run.sh
+++ auto_run.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 # crontab auto run this file
 # crontab command:
-# 59 22 * * 0  /path/to/your_script.sh
+# 59 22 * * 0  /path/to/your_script.sh  
 
 cd /home/ubuntu/git-commit-board
 git pull
 
 git add .
-today=`date +"%Y-%m-%d"`
-git commit -m "board update test**  $today"
-git push origin main
+
+today=$(date +"%Y-%m-%d")
+
+# commit message 
+commit_message="board update test** $today"
+
+git commit -m "$commit_message"
